8f6d68c8c254d055caaf199dabca4cc613ad2147
dheeraj <dheerajmattepally@gnail.com>
2026-04-16 14:59:51 +0530

    functions2

--- python_functions/functions_example2.py
+++ python_functions/functions_example2.py
@@ -14,4 +14,5 @@ def info(**data):
     for key,value in data.items():
         print("{}: {}".format(key,value))
 
-info(name='dheeraj',num=234567788,loaction='india',age=30)
+info(name='dheeraj',num=234567788,location='india',age=30)
+info(name='john',num=234567788,location='usa',age=30)
